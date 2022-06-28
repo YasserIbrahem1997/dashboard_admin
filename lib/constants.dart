31b21c52beab25cd3c6b9ee83d196ff4e65e8dbd
Eng. Yasser Ibrahem <89817238+engyasser1997@users.noreply.github.com>
2022-06-28 13:43:16 +0200

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF081730);
+const secondaryColor = Color(0xFF232A4E);
+const bgColor = Color(0xFF03112D);
+const bgColorCard= Color(0xD0F3E7DD);
+const bgColorDashbord= Color(0xFFF3EBEA);
+
+const defaultPadding = 16.0;
